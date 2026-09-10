197a8005b5fb269d89b4b307fdee463986886e0e
Matheesha <im.matheesha@gmail.com>
2026-09-10 08:49:46 +0530

    Update footer links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -264,17 +264,14 @@ export default function HomePage() {
       </div>
       {/* footer */}
       <div className='flex items-center bottom-0 left-0 fixed w-screen text-[10px] sm:text-[10px] md:text-[16px] h-fit p-3 border-t dark:bg-gray-950 dark:text-white dark:border-gray-800 bg-white border-gray-200'>
-        <p className='w-fit'>Made by <a href='https://github.com/ItsMatheesha' className='text-[#2663fa]'>Matheesha</a></p>
+        <p className='w-fit'>Made by <a href='https://github.com/m4theesha' className='text-[#2663fa]'>Matheesha</a></p>
         <div className='flex space-x-3 justify-end items-center ml-auto'>
           <p className='text-gray-500'>Built with <a href='https://react.dev/' className='text-[#2663fa]'>React</a>, <a href='https://nextjs.org/' className='text-[#2663fa]'>Next.js</a>, <a href='https://www.npmjs.com/package/@monaco-editor/react' className='text-[#2663fa]'>Monaco Editor</a>, and <a href='https://github.com/4zeroiv/jwt' className='text-[#2663fa]'>JWT library</a> by <a href='https://github.com/ItsMatheesha' className='text-[#2663fa]'>Matheesha</a></p>
-          <a href='https://github.com/ItsMatheesha/jwt'>
+          <a href='https://github.com/m4theesha/jwtkn'>
             <img src='https://skills.syvixor.com/api/icons?i=github' alt='github' width='40px' height='40px' />
           </a>
-          <a href='https://www.npmjs.com/package/@matheesha/jwt'>
+          <a href='https://www.npmjs.com/package/jwtkn'>
             <img src='https://skills.syvixor.com/api/icons?i=npm' alt='github' width='40px' height='40px' />
-          </a>
-          <a href='https://jsr.io/@matheesha/jwt'>
-            <img src='https://skills.syvixor.com/api/icons?i=jsr' alt='github' width='40px' height='40px' />
           </a>
         </div>
       </div>
